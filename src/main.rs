79f971e85da34a283493c799ac7da7e5ed9a7178
AidenJastrzembski <aidenjastrzembski@gmail.com>
2026-01-25 17:22:51 -0500

    add bleak

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,31 @@ struct Args {
 struct Theme {
     wallpaper: &'static str,
     border_color_focused: &'static str,
+    is_animated: bool,
 }
 
 const ZEN: Theme = Theme {
     wallpaper: "zen.jpg",
     border_color_focused: "0x67c77c",
+    is_animated: false,
 };
 
 const HIDEOUT: Theme = Theme {
     wallpaper: "hideout.png",
     border_color_focused: "0x8ce8ff",
+    is_animated: false,
 };
 
 const FREAK: Theme = Theme {
     wallpaper: "freak.jpg",
     border_color_focused: "0xff79c6",
+    is_animated: false,
+};
+
+const BLEAK: Theme = Theme {
+    wallpaper: "bleak.gif",
+    border_color_focused: "0xc9c9c9",
+    is_animated: true,
 };
 
 static WALLPAPER_ROOT: &str = "/home/aiden/.config/wallpapers/";
@@ -48,6 +58,7 @@ fn main() {
             "hatsune" => todo!(),
             "zen" => change_theme(ZEN),
             "freak" => change_theme(FREAK),
+            "bleak" => change_theme(BLEAK),
             _ => print!("Unknown theme: {}\n", theme),
         }
     }
@@ -58,23 +69,46 @@ fn list_themes() {
     print!("\tZen - Dream bedroom, kanagawa\n");
     print!("\tHideout - Cool hideout, nostalgic, rose-pine-moon\n");
     print!("\tFreak - Girl drinking energy drinks, tokyonight-storm\n");
+    print!("\tBleak - Lonely animated town, vague\n");
 }
 
 fn change_theme(theme: Theme) {
     Command::new("pkill").arg("swaybg").status().ok();
     println!("Killed swaybg");
 
+    // see which one we actually need
+    Command::new("pkill").arg("swww").status().ok();
+    Command::new("pkill")
+        .arg("swww-daemon")
+        .stdout(Stdio::null()) // tell swaybg to shut the fuck up
+        .stderr(Stdio::null())
+        .status()
+        .ok();
+    println!("Killed swww");
+
     let wallpaper = WALLPAPER_ROOT.to_owned() + &theme.wallpaper;
     println!("Wallpaper: {}", wallpaper);
 
-    Command::new("swaybg")
-        .arg("-i")
-        .arg(wallpaper)
-        .stdout(Stdio::null()) // tell swaybg to shut the fuck up
-        .stderr(Stdio::null())
-        .spawn()
-        .unwrap();
-    println!("Spawned swaybg");
+    if theme.is_animated {
+        Command::new("swww-daemon").spawn().unwrap();
+        Command::new("swww")
+            .arg("img")
+            .arg(wallpaper)
+            .stdout(Stdio::null()) // tell swaybg to shut the fuck up
+            .stderr(Stdio::null())
+            .spawn()
+            .unwrap();
+        println!("Spawned swww");
+    } else {
+        Command::new("swaybg")
+            .arg("-i")
+            .arg(wallpaper)
+            .stdout(Stdio::null()) // tell swaybg to shut the fuck up
+            .stderr(Stdio::null())
+            .spawn()
+            .unwrap();
+        println!("Spawned swaybg");
+    }
 
     // change focused border color
     Command::new("riverctl")
